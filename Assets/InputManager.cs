efd9425db3895b4f0f6e3f50b2298e77c1f8424f
hyyryaapolainen <a.hyyrylainen79@gmail.com>
2021-01-22 12:51:07 +0200

    Added player movemement

---
+++ Assets/InputManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public Vector3 _directionInput { get;  protected set; }
+    public bool sprintInput { get;  protected set;  }
+    public bool dodgeInput { get; protected set; }
+    public bool attackInput { get;  protected set; }
+    
+    
+    public virtual void OnEnable()
+    {
+        
+    }
+
+    public virtual void OnDisable()
+    {
+        
+    }
+}
---
+++ Assets/Movement.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+
+[RequireComponent(typeof(InputManager))]
+[RequireComponent(typeof(CharacterController))]
+public class Movement : MonoBehaviour
+{
+    protected CharacterController _characterController;
+    protected InputManager _inputManager;
+    
+    //Get from entity stats
+    [SerializeField]
+    private float movementSpeed = 10f;
+    [SerializeField]
+    private float sprintSpeed = 15f;
+    
+    
+    [SerializeField]
+    protected float turnSmoothing = 0.2f;
+    [SerializeField]
+    protected float sprintSmoothing = 0.1f;
+
+    protected float _currentMovementSpeed;
+    protected float _currentSmoothing;
+    protected float _turnSmoothVelocity;
+    private void Start()
+    {
+        _inputManager = GetComponent<InputManager>();
+        _characterController = GetComponent<CharacterController>();
+        _currentMovementSpeed = movementSpeed;
+        _currentSmoothing = turnSmoothing;
+    }
+    private void FixedUpdate()
+    {
+        if (_inputManager._directionInput.magnitude >= 0.1f)
+        {
+            Move();
+        }
+
+        if (_inputManager.sprintInput)
+        {
+            Sprint();
+        }
+        else
+        {
+            _currentMovementSpeed = movementSpeed;
+            turnSmoothing = _currentSmoothing;
+        }
+    }
+    protected virtual void Sprint()
+    {
+        _currentMovementSpeed = sprintSpeed;
+        turnSmoothing = sprintSmoothing;
+    }
+
+    protected virtual void Move()
+    {
+
+    }
+}
