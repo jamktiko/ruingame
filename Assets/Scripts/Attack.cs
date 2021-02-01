8ed93150288c368727d9996675e2c8356e4fcfbc
hyyryaapolainen <a.hyyrylainen79@gmail.com>
2021-02-01 12:04:38 +0200

    Added basic damage collider

--- Assets/Scripts/Attack.cs
+++ Assets/Scripts/Attack.cs
@@ -8,9 +8,12 @@ public class Attack : MonoBehaviour
     [SerializeField]
     private InputReader _inputReader;
 
+    [SerializeField] private DamageCollider _damageCollider;
+
     [SerializeField] private float _damage = 100f;
 
     [SerializeField] private float _attackDistance = 3f;
+    
     //Get Attack Input
         //Check if already attacking
         //Check Combo Step
@@ -19,44 +22,34 @@ public class Attack : MonoBehaviour
             //Trigger Animation, prevent attacking while an attack is in progress
                 //Last Combo Step resets number
     
-    
+   
     private void OnEnable()
     {
-        _inputReader.attackEvent += ExecuteAttacK;
+        _inputReader.attackEvent += ExecuteAttack;
     }
 
     private void OnDisable()
     {
-        _inputReader.attackEvent -= ExecuteAttacK;
+        _inputReader.attackEvent -= ExecuteAttack;
     }
 
-    private void ExecuteAttacK()
+    private void ExecuteAttack()
     {
+        _damageCollider.EnableDamage();
         Debug.Log("Executing Attack!");
     }
     void EnableDamage()
     {
-        Debug.Log("Enabling Damage!");
-        RaycastHit hit;
         
-        //Checks for enemies in range and applies damage
+    }
+
+    void DisableDamage()
+    {
         
-        if (Physics.SphereCast(transform.position, transform.lossyScale.x / 2, transform.forward, out hit,
-            _attackDistance))
-        {
-            Debug.Log(hit.collider.gameObject.name);
-            try
-            {
-                hit.collider.gameObject.GetComponent<Health>().DealDamage(_damage);
-            }
-            catch
-            { 
-                
-            }
-        }
     }
 
     //Attack Range visualization
+    /*
    private void OnDrawGizmos()
     {
         Gizmos.DrawWireCube(transform.forward * _attackDistance/2, new Vector3(3, transform.lossyScale.y / 2, _attackDistance));
@@ -76,5 +69,5 @@ public class Attack : MonoBehaviour
             Gizmos.DrawRay(transform.position, transform.forward * maxDistance);
         }
     }
-    
+    */
 }
---
+++ Assets/Scripts/DamageCollider.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageCollider : MonoBehaviour
+{
+    [SerializeField] private Collider _damageCollider;
+    [SerializeField] private float _damage;
+    private void OnTriggerEnter(Collider collision)
+    {
+        Debug.Log(collision);
+        if (collision.tag == "Enemy")
+        {
+            var _targetHealth = collision.gameObject.GetComponent<Health>();
+            Debug.Log(_targetHealth);
+            if (_targetHealth != null)
+                _targetHealth.DealDamage(_damage);
+        }
+    }
+
+    public void EnableDamage()
+    {
+        _damageCollider.enabled = true;
+    }
+
+    public void DisableDamage()
+    {
+        _damageCollider.enabled = false;
+    }
+}
