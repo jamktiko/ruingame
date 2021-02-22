4321b98dee4e5eafd9fe94232f0df18ba8c9aaa7
DESKTOP-FNNVPOE\- <200924@gradia.fi>
2021-02-22 11:15:21 +0200

    Health: Changed protection level for resistances. PlayerHealth: Encapsulated resistances

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerHealth : Health
 {
+    public float FlatResistance { get => _flatResistance; set => _flatResistance = value; }
+    public float PercentualResistance { get => _percentualResistance; set => _percentualResistance = value; }
     public override void Die()
     {
         PlayerManager pm = GetComponent<PlayerManager>();
