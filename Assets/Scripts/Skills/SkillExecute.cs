91ba73d6b1f5bbf631236642299ad64063690342
DESKTOP-FNNVPOE\- <200924@gradia.fi>
2021-02-22 11:01:14 +0200

    Added ModifyPlayerStats method, moved coroutines to base method, added skill parameters and Targeting instance

--- Assets/Scripts/Skills/SkillExecute.cs
+++ Assets/Scripts/Skills/SkillExecute.cs
@@ -5,24 +5,46 @@ using UnityEngine;
 public class SkillExecute : MonoBehaviour
 {
 
+    public SkillUser skillUser;
     public string skillname;
     public string skillDescription;
     public float skillCooldown = 3f;
     public float iFrameDuration = 0.3f;
     public bool onCooldown = false;
-    public SkillUser skillUser;
     public float duration = 0.5f;
+
+    [SerializeField] protected bool attackAllEnemies = true;
+    [SerializeField] protected float attackRadius = 3f;
+    [SerializeField] protected float attackDistance = 0f;
+    [SerializeField] protected float damage = 10f;
+    [SerializeField] protected float SprintSpeed = 20f;
+
+    protected Targeting targeting;
+
+
+    private void Start()
+    {
+        targeting = gameObject.AddComponent<Targeting>();
+
+    }
     public virtual void Execute()
     {
     }
-    
-    public virtual void ApplyPersistentEffect()
+
+    public virtual void ApplyPersistentEffect(SkillExecute sk)
     {
-        
+        IEnumerator coroutine = skillUser.UsePersistentEffect(sk);
+        skillUser.StartCoroutine(coroutine);
+        IEnumerator coroutine2 = skillUser.GoOnCooldown(sk);
+        skillUser.StartCoroutine(coroutine2);
     }
 
     public virtual void DeActivatePersistentEffect()
     {
-        
+
+    }
+    public virtual void ModifyPlayerStats(int type)
+    {
+
     }
 }
