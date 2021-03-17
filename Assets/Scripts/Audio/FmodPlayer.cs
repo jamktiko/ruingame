1c43283d9bd2d0a7ffb3ae674e0b13e6a5564759
Anssi <zizzna93@gmail.com>
2021-03-17 15:19:10 +0200

    FMOD folders added

---
+++ Assets/Scripts/Audio/FmodPlayer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FmodPlayer : MonoBehaviour
+{
+    void PlaySound(string path)
+    {
+        FMODUnity.RuntimeManager.PlayOneShot(path, GetComponent<Transform>().position);
+    }
+}
