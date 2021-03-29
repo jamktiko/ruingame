b3d40dfd1a175beee1eff5536d775b4d85072068
DESKTOP-FNNVPOE\- <200924@gradia.fi>
2021-03-29 09:38:10 +0300

    Added BookOfTheWiseOnes artifact

---
+++ Assets/Scripts/Items/BookOfTheWiseOnes.cs
@@ -0,0 +1,29 @@
+﻿
+using DefaultNamespace;
+using UnityEngine;
+using System.Collections;
+
+public class BookOfTheWiseOnes : ArtifactEffect
+{
+    public float blockedDamage = 10f;
+    private PlayerHealth _playerHealth;
+    private float maxHealthModifier = 1.15f;
+    private float maxHealth;
+
+    public override void AddEffect()
+    {
+        _playerReference = PlayerManager.Instance;
+
+        if (_playerReference.TryGetComponent(out _playerHealth))
+        {
+            maxHealth = _playerHealth._maximumHealth;
+            _playerHealth._maximumHealth *= maxHealthModifier;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (_playerHealth._maximumHealth > maxHealth)
+            _playerHealth._maximumHealth /= maxHealthModifier;
+    }
+}
