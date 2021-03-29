bfe6dd4da1c113dc943bb5d335d15ab9c9418f34
DESKTOP-FNNVPOE\- <200924@gradia.fi>
2021-03-29 10:07:47 +0300

    Added TheRuinedRing artifacts

---
+++ Assets/Scripts/Items/TheRuinedRing.cs
@@ -0,0 +1,26 @@
+﻿
+using DefaultNamespace;
+using UnityEngine;
+using System.Collections;
+
+public class TheRuinedRing : ArtifactEffect
+{
+    private float cooldownModifier = 0.95f;
+    private float firstCooldownTime;
+
+    public override void AddEffect()
+    {
+       _playerReference = PlayerManager.Instance;
+        firstCooldownTime = _playerReference._playerSkills.skillList[0].skillCooldown;
+
+        foreach (var skill in _playerReference._playerSkills.skillList)
+            skill.skillCooldown *= cooldownModifier;
+    }
+
+    private void OnDestroy()
+    {
+        if (firstCooldownTime > _playerReference._playerSkills.skillList[0].skillCooldown)
+            foreach (var skill in _playerReference._playerSkills.skillList)
+                skill.skillCooldown /= cooldownModifier;
+    }
+}
