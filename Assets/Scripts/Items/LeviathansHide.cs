7a90a236a1f4324cab2d07006bc5459a2c6def1e
DESKTOP-FNNVPOE\- <200924@gradia.fi>
2021-03-29 09:54:37 +0300

    Added KappasPlate and LeviathansHide artifacts

---
+++ Assets/Scripts/Items/LeviathansHide.cs
@@ -0,0 +1,25 @@
+﻿
+using DefaultNamespace;
+using UnityEngine;
+using System.Collections;
+
+public class LeviathansHide : ArtifactEffect
+{
+    public float blockedDamage = 10f;
+    private PlayerHealth _playerHealth;
+
+    public override void AddEffect()
+    {
+        _playerReference = PlayerManager.Instance;
+
+        if (_playerReference.TryGetComponent(out _playerHealth))
+        {
+            _playerHealth.flatResistance += blockedDamage;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        _playerHealth.flatResistance -= blockedDamage;
+    }
+}
