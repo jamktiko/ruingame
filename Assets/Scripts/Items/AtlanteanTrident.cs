09a194069b87b8a3c18cc9a9b727a79cb44403f9
DESKTOP-FNNVPOE\- <200924@gradia.fi>
2021-03-29 09:35:16 +0300

    Added AtlanteanTrident artifact

---
+++ Assets/Scripts/Items/AtlanteanTrident.cs
@@ -0,0 +1,47 @@
+﻿
+using DefaultNamespace;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AtlanteanTrident : ArtifactEffect
+{
+    public float slowDownTime = 2f;
+    PlayerAttackHandler playerAttackHandler;
+    private Movement enemyMovement;
+    private float normalEnemySpeed;
+
+    private void Start()
+    {
+        _playerReference = PlayerManager.Instance;
+        normalEnemySpeed = gameObject.AddComponent<Movement>().movementSpeed;
+    }
+
+    public override void AddEffect()
+    {
+        if (_playerReference.TryGetComponent(out playerAttackHandler))
+        {
+            playerAttackHandler.PlayerAttackEvent += SlowEnemyDown;
+        }
+    }
+
+    private void SlowEnemyDown(float damage, Health enemyHealth)
+    {
+        if (enemyHealth.TryGetComponent(out enemyMovement))
+        {
+            enemyMovement.movementSpeed = normalEnemySpeed / 2f;
+            float time = enemyHealth.GetComponent<StunnedState>().StunTimer + slowDownTime;
+            Invoke("NormilizeMovementSpeed", time);
+        }
+    }
+
+    void NormilizeMovementSpeed()
+    {
+        enemyMovement.movementSpeed = normalEnemySpeed;
+    }
+
+    private void OnDestroy()
+    {
+        playerAttackHandler.PlayerAttackEvent -= SlowEnemyDown;
+    }
+}
