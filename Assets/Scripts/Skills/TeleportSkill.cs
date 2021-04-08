fdc11b2ea486e74cebceb7898111148cb636781c
DESKTOP-FNNVPOE\- <200924@gradia.fi>
2021-04-08 11:02:07 +0300

    Added teleport to TeleportSkill

--- Assets/Scripts/Skills/TeleportSkill.cs
+++ Assets/Scripts/Skills/TeleportSkill.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using UnityEngine;
 public class TeleportSkill : SkillExecute
 {
     public float teleportDistance = 10f;
@@ -6,5 +6,16 @@ public class TeleportSkill : SkillExecute
     public override void Execute(float duration)
     {
         var tr = skillUser.transform;
+        tr.position += transform.forward * TeleportDistance();
+    }
+
+    float TeleportDistance()
+    {
+        LayerMask layer = LayerMask.GetMask("CameraCollision");
+        if (Physics.CapsuleCast(transform.position, transform.position + Vector3.up * 1f, .4f, transform.forward, out RaycastHit hit, teleportDistance, layer, QueryTriggerInteraction.Collide))
+        {
+            return hit.distance - 0.7f;
+        }
+        return teleportDistance;
     }
 }
