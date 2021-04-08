944c7c73f2bf3c2122a98bcdd3bcb7b6a0828af5
DESKTOP-FNNVPOE\- <200924@gradia.fi>
2021-04-08 11:22:42 +0300

    Code cleanup

--- Assets/Scripts/Skills/TeleportSkill.cs
+++ Assets/Scripts/Skills/TeleportSkill.cs
@@ -2,8 +2,8 @@
 public class TeleportSkill : SkillExecute
 {
     public float teleportDistance = 10f;
-    
-    public override void Execute(float duration)
+
+    public override void Execute()
     {
         var tr = skillUser.transform;
         tr.position += transform.forward * TeleportDistance();
@@ -12,7 +12,9 @@ public class TeleportSkill : SkillExecute
     float TeleportDistance()
     {
         LayerMask layer = LayerMask.GetMask("CameraCollision");
-        if (Physics.CapsuleCast(transform.position, transform.position + Vector3.up * 1f, .4f, transform.forward, out RaycastHit hit, teleportDistance, layer, QueryTriggerInteraction.Collide))
+        float capsuleRadius = 0.4f;
+        float capsuleHeight = 1f;
+        if (Physics.CapsuleCast(transform.position, transform.position + Vector3.up * capsuleHeight, capsuleRadius, transform.forward, out RaycastHit hit, teleportDistance, layer, QueryTriggerInteraction.Collide))
         {
             return hit.distance - 0.7f;
         }
