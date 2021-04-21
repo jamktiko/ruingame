78c1fb94d85eabbc09ea10cf87d2f76d965d668b
DESKTOP-FNNVPOE\- <200924@gradia.fi>
2021-04-21 11:03:53 +0300

    Added check if player touches wall

--- Assets/Scripts/Player/MovementController.cs
+++ Assets/Scripts/Player/MovementController.cs
@@ -269,14 +269,17 @@ public class MovementController : MonoBehaviour
     public virtual IEnumerator Dashing2(float duration)
     {
         Vector3 startPos = transform.position;
-        float capsuleMaxHeight = 1.99f;
-        float capsuleMaxRadius = 0.49f;
+        float capsuleMaxHeight = 2f;
+        float capsuleMaxRadius = 0.5f;
         float maxDistance = 10f;
         CapsuleCollider[] colliders = GetComponents<CapsuleCollider>();
         Vector3 direction = DashDirection();
-
-        CheckDashHitOnWalls(ref maxDistance, capsuleMaxHeight, capsuleMaxRadius, direction);
-        CheckOverlaps(ref maxDistance, startPos, 2f, 0.8f);
+        
+        if (!CheckIfPlayerTouchesWall(ref maxDistance, capsuleMaxHeight, capsuleMaxRadius))
+        {
+            CheckDashHitOnWalls(ref maxDistance, capsuleMaxHeight, capsuleMaxRadius, direction);
+            CheckOverlaps(ref maxDistance, startPos, 2f, 0.8f);
+        }
         FreezePosition(true);
         EneableColliders(false, colliders);
 
@@ -289,7 +292,6 @@ public class MovementController : MonoBehaviour
         }
         dashing = false;
         _characterAnimator.SetBool("dashing", false);
-        Debug.Log(Vector3.Distance(startPos, transform.position));
         EneableColliders(true, colliders);
         FreezePosition(false);
     }
@@ -329,6 +331,24 @@ public class MovementController : MonoBehaviour
         {
             item.enabled = enable;
         }
+    }
+
+    private bool CheckIfPlayerTouchesWall(ref float maxDistance, float capsuleMaxHeight, float capsuleMaxRadius)
+    {
+        Vector3 p1 = transform.position;
+        Vector3 p2 = transform.position + Vector3.up * capsuleMaxHeight;
+        LayerMask layer = LayerMask.GetMask("CameraCollision");
+        Collider[] colliders = Physics.OverlapCapsule(p1, p2, capsuleMaxRadius, layer, QueryTriggerInteraction.Collide);
+
+        foreach (var item in colliders)
+        {
+            if (item.transform.tag != "Ground")
+            {
+                maxDistance = 0f;
+                return true;
+            }
+        }
+        return false;
     }
 
     private void CheckDashHitOnWalls(ref float maxDistance,  float capsuleMaxHeight, float capsuleMaxRadius, Vector3 direction)
