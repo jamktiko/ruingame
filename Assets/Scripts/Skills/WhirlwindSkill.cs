e6de59cbea6f6ce29a6ddd8221479ad3e6dd382b
DESKTOP-FNNVPOE\- <200924@gradia.fi>
2021-04-26 11:44:21 +0300

    Modified Whirlwind to use MeleeAttack

--- Assets/Scripts/Skills/WhirlwindSkill.cs
+++ Assets/Scripts/Skills/WhirlwindSkill.cs
@@ -26,17 +26,23 @@ namespace DefaultNamespace.Skills
     public class WhirlwindSkill : SkillExecute
     {
 
-        [SerializeField] private float _attackRadius = 50f;
+        [SerializeField] private float _attackRadius = 5f;
         [SerializeField] private float _knockbackForce = 10f;
         private float _attackDistance;
+        private MeleeAttack whirlwind;
+        LayerMask enemyLayer;
+        BaseAttackHandler attackHandler;
 
         protected override void Start()
         {
             base.Start();
             skillname = "WhirlWind";
-            damage = 20f;
-            skillCooldown = 5f;
+            whirlwind = Whirlwind();
+            damage = 50f;
+            skillCooldown = 1f;
             _attackDistance = _attackRadius / 2f;
+            enemyLayer = LayerMask.GetMask("EnemyLayer");
+            attackHandler = GetComponent<BaseAttackHandler>();
         }
 
         public override void Execute(float duration)
@@ -44,27 +50,50 @@ namespace DefaultNamespace.Skills
             skillUser.usingSkill = true;
             iFrameDuration = duration;
             base.Execute(duration);
-            DamageAndKnockback();
+            ExecuteWhirlWind();
             skillUser.usingSkill = false;
         }
 
-        private void DamageAndKnockback()
+        private void ExecuteWhirlWind()
+        {
+            GameObject[] gos = FindEnemies();
+            if (gos.Length > 0)
+            {
+                try
+                {
+                    whirlwind.AttackAllTargets(gos, attackHandler);
+                    foreach (var enemyGo in gos)
+                    {
+                        Enemy_StateMachine enemy = enemyGo.GetComponent<Enemy_StateMachine>();
+                        enemy.SetState(new StunnedState(enemy));
+                    }
+                }
+                catch { Debug.Log("couldn't execute whirlwind"); }
+            }
+        }
+
+        GameObject[] FindEnemies()
         {
             Vector3 p1 = transform.position + transform.forward * _attackDistance;
             Vector3 p2 = transform.position + transform.forward * _attackDistance + Vector3.up * 2f;
-            Collider[] colliders = targeting.FindColliderOverlaps(p1, p2, _attackRadius);
-            foreach (var collider in colliders)
+            Collider[] colliders = Physics.OverlapCapsule(p1, p2, _attackRadius, enemyLayer, QueryTriggerInteraction.Collide);
+            GameObject[] enmyGos = new GameObject[colliders.Length];
+
+            for (int i = 0; i < colliders.Length; i++)
             {
-                if (collider.gameObject.TryGetComponent(out BaseMovement kbh))
-                {
-                    kbh.HandleKnockBack(transform.position, _knockbackForce);
-                }
-                if (collider.gameObject.TryGetComponent(out Enemy_StateMachine enemy))
-                {
-                    enemy.SetState(new StunnedState(enemy));
-                }
-                targeting.DamageEnemy(collider.gameObject, damage);
+                enmyGos[i] = colliders[i].gameObject;
             }
+            return enmyGos;
+        }
+
+        public MeleeAttack Whirlwind()
+        {
+            var _whirlwind = ScriptableObject.CreateInstance<MeleeAttack>();
+            _whirlwind.DamageType = baseDamageType.DIRECT;
+            _whirlwind.baseDamage = damage;
+            _whirlwind.KnockBack = true;
+            _whirlwind.KnockBackStrength = _knockbackForce;
+            return _whirlwind;
         }
 
     }
