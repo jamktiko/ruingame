24c6226961e7eeba61474865c4070350acc08316
DESKTOP-FNNVPOE\- <200924@gradia.fi>
2021-05-04 15:35:16 +0300

    Added DontDestroyOnLoad GameObject for spawned artifacts

---
+++ Assets/Scripts/Items/SpawnedArtifacts.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DefaultNamespace;
+public class SpawnedArtifacts : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static SpawnedArtifacts Instance;
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void DestroyGO()
+    {
+
+        Destroy(this.gameObject);
+
+    }
+}
