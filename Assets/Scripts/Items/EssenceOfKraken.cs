6a17aadde4e460718645f4fb209ff0b692033e79
DESKTOP-FNNVPOE\- <200924@gradia.fi>
2021-05-04 15:58:09 +0300

    Modified EssenceOfKraken

--- Assets/Scripts/Items/EssenceOfKraken.cs
+++ Assets/Scripts/Items/EssenceOfKraken.cs
@@ -3,22 +3,11 @@ using DefaultNamespace;
 
 public class EssenceOfKraken : ArtifactEffect
 {
-    //private SprintSkill sprintSkill;
     public override void AddEffect()
     {
         _playerReference = PlayerManager.Instance;
         base.AddEffect();
-        _playerReference._playerSkills.skillList[3].skillCooldown *= 0.6f;
-        //sprintSkill = _playerReference.GetComponent<SprintSkill>();
-        //sprintSkill = _playerReference.GetComponent<SprintSkill>();
-        //sprintSkill.skillCooldown *= 0.6f;
-    }
-
-    private void OnDestroy()
-    {
-        artifactModifiers[0].type = ArtifactModifier.Type.Minus;
-        base.AddEffect();
-        _playerReference._playerSkills.skillList[3].skillCooldown /= 0.6f;
-        //sprintSkill.skillCooldown /= 0.6f;
+        SprintSkill sprintSkill = _playerReference.GetComponent<SprintSkill>();
+        sprintSkill.skillCooldown *= 0.6f;
     }
 }
